8ac467a54fc1e7bf3cfaa27312d75a7a3b60db32
jincheol2578 <jincheol2578@gmail.com>
2021-07-07 00:24:23 +0900

    21.07.06 package 구조, DB 설계 끝

---
+++ backend/ToDoList/src/main/java/com/project/todolist/domain/project/service/SectionService.java
@@ -0,0 +1,7 @@
+package com.project.todolist.domain.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SectionService {
+}
---
+++ backend/ToDoList/src/main/java/com/project/todolist/domain/project/service/TaskService.java
@@ -0,0 +1,9 @@
+package com.project.todolist.domain.project.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.xml.ws.ServiceMode;
+
+@Service
+public class TaskService {
+}
