20e9614648890ab1e150082dccdc622d85b789b4
scseong <goidtv@naver.com>
2021-08-24 16:49:26 +0900

    210824 프론트엔드 회원가입, 로그인 추가

---
+++ frontend/layouts/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+const Channel = loadable(() => import('@pages/Channel'));
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/workspace/channel" component={Channel} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
--- frontend/src/component/Calendar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Clock from 'react-live-clock';
-
-export default function Calendar() {
-  return (
-    <Clock format={"dddd, MMMM DD YYYY"} ticking={true} timezone={'	Etc/GMT+9'} />
-  )
-}
